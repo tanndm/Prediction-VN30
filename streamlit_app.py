7ba207ba370cbb73f99f70b6704ef554d3e9ff81
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-03-15 10:06:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import joblib
 # Importing all required modules 
 import webbrowser as wb
 import streamlit as st
+import time
 
 # -- Set page config
 apptitle = 'Predict VN30-index price movement using financial news and technical analysis'
