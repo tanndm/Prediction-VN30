1be2377a7747579ae1ff1d22017999f5911058de
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-03-15 14:09:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,7 +156,7 @@ if option == 'Manual Input':
       with st.spinner('Wait for it...'):
         time.sleep(3)
       st.info('Next day, VN30-index will uptrend', icon="ℹ️")
-      st.info(f'Next day, VN30-index will uptrend {pred}', icon="ℹ️")
+      st.info(f'Next day, {pred_out(pred)}', icon="ℹ️")
 else:
   uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
   for uploaded_file in uploaded_files:
