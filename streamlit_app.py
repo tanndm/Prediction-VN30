ca7cb5dfae42783bdafc30aa1f66aefafd88b105
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-16 23:53:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -169,69 +169,10 @@ if click_data:
   st.plotly_chart(fig2, theme="streamlit", use_container_width=True)
 
 
-select_event = st.sidebar.selectbox('How do you want to find data?',
-                                    ['By event name', 'By GPS'])
-##############################################################################################
-st.header("Report model")
-col00, col2, col3, col4, col5 = st.columns(5)
-with col00:
-  st.metric(label="", value="Label 0")
-with col2:
-  st.metric(label="Precison label 0", value="73%")
-with col3:
-  st.metric(label="Recall label 0", value="62%")
-with col4:
-  st.metric(label="F1-score", value="67%")
-with col5:
-  st.metric(label="Support", value="142")
+select_event = st.sidebar.selectbox('Methods',
+                                    ['Manual input', 'Upload file'])
 
-col01, col6, col7, col8, col9 = st.columns(5)
-with col01:
-  st.metric(label="", value="Label 1")
-with col6:
-  st.metric(label="Precison label 1", value="70%")
-with col7:
-  st.metric(label="Recall label 1", value="80%")
-with col8:
-  st.metric(label="F1-score", value="75%")
-with col9:
-  st.metric(label="Support", value="161")
-
-col13, col1, col10, col11, col12 = st.columns(5)
-with col10:
-  st.metric(label="Accuracy", value="71%")
-with col11:
-  st.metric(label="F1-score", value="71%")
-with col12:
-  st.metric(label="Support", value="303")  
-
-import datetime
-
-# d = st.date_input(
-#     "When\'s your birthday",
-#     datetime.date(2019, 7, 6))
-# st.write('Your birthday is:', d)
-
-st.header("Report model")
-cold1, cold2 = st.columns(2)
-with cold1:
-  d = st.date_input(
-    "Start: ",
-    datetime.date(2019, 7, 6))
-with cold2:
-  d2 = st.date_input(
-    "End: ",
-    datetime.date(2023, 4, 4))
-
-########################################################################
-option = st.selectbox(
-    '**How would you like to be input data?**',
-    ('Automatic','Manual Input', 'Upload a file'))
-
-if option == 'Manual Input':
-#   url = "https://finance.vietstock.vn/phan-tich-ky-thuat.htm#"
-#   if st.button('Click here to have exactly data of technical analysis ratio'):
-#     st.write(f"Finance.Vietstock analysis link: {url}")
+if select_event == 'Manual input':
   with st.form("my_form"):
     st.title("Historical data") 
     # historical data
@@ -304,17 +245,7 @@ if option == 'Manual Input':
         with st.spinner('Wait for it...'):
           time.sleep(1)
         st.warning('You do not input enough neccessary features', icon="⚠️")
-    
-#     submitted_2 = st.form_submit_button("Get result") 
-#     if submitted_2:     
-#       with st.spinner('Wait for it...'):
-#         time.sleep(3)
-#       pred_out(pred)
-#     else:
-#       st.warning('You do not input enough neccessary features', icon="⚠️")
-
-    
-elif option == 'Upload a file':
+else:
   sample_df = pd.DataFrame({'Number of buy orders': 66774, 'Buy-orders volume':196533544, 'Number of sell orders':58645, 'Sell-orders volume':199406752,
                            'Order matching volume':-2872869, 'Put-through volume':107108336, 'Positive':1, 'Negative':0,
                            'SMA_10':1020, 'SMA_20':1019, 'EMA_10':1020, 'EMA_20':1019, 'RSI_7d':56, 
@@ -326,8 +257,164 @@ elif option == 'Upload a file':
   for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-else:
-    pass
+    
+##############################################################################################
+st.header("Report model")
+col00, col2, col3, col4, col5 = st.columns(5)
+with col00:
+  st.metric(label="", value="Label 0")
+with col2:
+  st.metric(label="Precison label 0", value="73%")
+with col3:
+  st.metric(label="Recall label 0", value="62%")
+with col4:
+  st.metric(label="F1-score", value="67%")
+with col5:
+  st.metric(label="Support", value="142")
+
+col01, col6, col7, col8, col9 = st.columns(5)
+with col01:
+  st.metric(label="", value="Label 1")
+with col6:
+  st.metric(label="Precison label 1", value="70%")
+with col7:
+  st.metric(label="Recall label 1", value="80%")
+with col8:
+  st.metric(label="F1-score", value="75%")
+with col9:
+  st.metric(label="Support", value="161")
+
+col13, col1, col10, col11, col12 = st.columns(5)
+with col10:
+  st.metric(label="Accuracy", value="71%")
+with col11:
+  st.metric(label="F1-score", value="71%")
+with col12:
+  st.metric(label="Support", value="303")  
+
+import datetime
+
+# d = st.date_input(
+#     "When\'s your birthday",
+#     datetime.date(2019, 7, 6))
+# st.write('Your birthday is:', d)
+
+st.header("Report model")
+cold1, cold2 = st.columns(2)
+with cold1:
+  d = st.date_input(
+    "Start: ",
+    datetime.date(2019, 7, 6))
+with cold2:
+  d2 = st.date_input(
+    "End: ",
+    datetime.date(2023, 4, 4))
+
+########################################################################
+# option = st.selectbox(
+#     '**How would you like to be input data?**',
+#     ('Automatic','Manual Input', 'Upload a file'))
+
+# if option == 'Manual Input':
+# #   url = "https://finance.vietstock.vn/phan-tich-ky-thuat.htm#"
+# #   if st.button('Click here to have exactly data of technical analysis ratio'):
+# #     st.write(f"Finance.Vietstock analysis link: {url}")
+#   with st.form("my_form"):
+#     st.title("Historical data") 
+#     # historical data
+#     his1, his2, his3 = st.columns(3)
+#     with his1:
+#       bid_quality = st.number_input("Number of buy orders")
+#     with his2:
+#       bid_volume = st.number_input("Buy orders volume")
+#     with his3:
+#       ask_quality = st.number_input("Number of sell orders")
+      
+#     his4, his5, his6 = st.columns(3)
+#     with his4:
+#       ask_volume = st.number_input("Sell orders volume")
+#     with his5:
+#       matching_volume = st.number_input("Order matching volume")
+#     with his6:
+#       negotiable_volume = st.number_input("Put-through volume")
+    
+#     st.title("Financial news")
+#     dum1, dum2, ta1 = st.columns(3)
+#     with dum1:
+#       positive = st.number_input("Positive news", value=1)
+#     with dum2:
+#       negative = st.number_input("Negative news", value=0)
+#     with ta1:
+#       SMA_10_lag = st.number_input("SMA 10 days")  
+      
+#     st.title("Technical analysis")
+#     ta2, ta3, ta4 = st.columns(3)
+#     with ta2:
+#       SMA_20_lag = st.number_input("SMA 20 days")
+#     with ta3:
+#       EMA_10_lag = st.number_input("EMA 10 days")
+#     with ta4:
+#       EMA_20_lag = st.number_input("EMA 20 days")
+      
+#     ta5, ta6, ta7 = st.columns(3)
+#     with ta5:
+#       RSI_7d_lag = st.number_input("RSI 7 days")
+#     with ta6:
+#       RSI_9d_lag = st.number_input("RSI 9 days")
+#     with ta7:
+#       RSI_14d_lag = st.number_input("RSI 14 days")
+#     # Prediction
+#     features= [bid_quality, bid_volume, ask_quality, ask_volume, matching_volume, matching_volume,
+#                positive, negative, SMA_10_lag, SMA_20_lag, EMA_10_lag, EMA_20_lag, RSI_7d_lag, RSI_9d_lag, RSI_14d_lag]
+    
+#     res_df = pd.DataFrame({'bid_quality':bid_quality, 'bid_volume':bid_volume, 'ask_quality':ask_quality, 'ask_volume':ask_volume,
+#                            'matching_volume':matching_volume, 'negotiable_volume':negotiable_volume, 'Positive':positive, 'Negative':negative,
+#                            'SMA_10':SMA_10_lag, 'SMA_20':SMA_20_lag, 'EMA_10':EMA_10_lag, 'EMA_20':EMA_20_lag, 'RSI_7d':RSI_7d_lag, 
+#                            'RSI_9d':RSI_9d_lag, 'RSI_14d':RSI_14d_lag},index=["05-01-2023"])
+    
+#     pred = scaler.predict(np.array(features,ndmin=2))
+    
+#     submitted = st.form_submit_button("Forecast buttom") 
+#     check_data = False
+#     if submitted:     
+#       if (RSI_14d_lag != 0.00 and bid_quality != 0.00 and bid_volume != 0.00 and ask_quality != 0.00 and ask_volume != 0.00 
+#           and matching_volume != 0.00 and SMA_10_lag != 0.00 and SMA_20_lag != 0.00 and EMA_10_lag != 0.00 and EMA_20_lag != 0.00
+#           and RSI_7d_lag != 0.00 and RSI_9d_lag != 0.00 and RSI_14d_lag != 0.00):
+#         check_data = True
+#         with st.spinner('Wait for it...'):
+#           time.sleep(2)
+#         st.success('This is a success updating!', icon="✅")
+#         st.dataframe(res_df)
+#         pred_out(pred)
+#       else:
+#         check_data = False
+#         with st.spinner('Wait for it...'):
+#           time.sleep(1)
+#         st.warning('You do not input enough neccessary features', icon="⚠️")
+    
+# #     submitted_2 = st.form_submit_button("Get result") 
+# #     if submitted_2:     
+# #       with st.spinner('Wait for it...'):
+# #         time.sleep(3)
+# #       pred_out(pred)
+# #     else:
+# #       st.warning('You do not input enough neccessary features', icon="⚠️")
+
+    
+# elif option == 'Upload a file':
+#   sample_df = pd.DataFrame({'Number of buy orders': 66774, 'Buy-orders volume':196533544, 'Number of sell orders':58645, 'Sell-orders volume':199406752,
+#                            'Order matching volume':-2872869, 'Put-through volume':107108336, 'Positive':1, 'Negative':0,
+#                            'SMA_10':1020, 'SMA_20':1019, 'EMA_10':1020, 'EMA_20':1019, 'RSI_7d':56, 
+#                            'RSI_9d':55, 'RSI_14d':54},index=["dd-MM-YY"])
+#   st.write("Please upload data like the sample:")
+#   st.dataframe(sample_df)
+
+#   uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+#   for uploaded_file in uploaded_files:
+#     bytes_data = uploaded_file.read()
+#     st.write("filename:", uploaded_file.name)
+# else:
+#     pass
 
 
 
