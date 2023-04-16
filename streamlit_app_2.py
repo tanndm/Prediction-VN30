a364dcb451567e41d31585b95bb69b57f3e0cdc2
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-16 23:45:04 +0700

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -29,16 +29,21 @@ st.title('Application :blue[Deep Learning] and :red[Machine Learning] in predict
 ###############################################################################################
 import plotly.graph_objects as go
 df = pd.read_csv('vn30-his-2.csv')
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.set_index('Date')
 
 time_periods = {
-    '1 week': pd.date_range(end=df['Date'].iloc[-1], periods=5, freq=pd.tseries.offsets.BDay()),
-    '1 month': pd.date_range(end=df['Date'].iloc[-1], periods=20, freq=pd.tseries.offsets.BDay()),
-    '6 months': pd.date_range(end=df['Date'].iloc[-1], periods=120, freq=pd.tseries.offsets.BDay()),
-    '1 year': pd.date_range(end=df['Date'].iloc[-1], periods=252, freq=pd.tseries.offsets.BDay()),
-    '5 years': pd.date_range(end=df['Date'].iloc[-1], periods=1260, freq=pd.tseries.offsets.BDay())
+    '5 years': pd.date_range(end=df.index[-1], periods=1260, freq=pd.tseries.offsets.BDay()),
+    '1 year': pd.date_range(end=df.index[-1], periods=252, freq=pd.tseries.offsets.BDay()),
+    '6 months': pd.date_range(end=df.index[-1], periods=120, freq=pd.tseries.offsets.BDay()),
+    '3 month': pd.date_range(end=df.index[-1], periods=60, freq=pd.tseries.offsets.BDay()),
+    '1 month': pd.date_range(end=df.index[-1], periods=20, freq=pd.tseries.offsets.BDay()),
+    '2 week': pd.date_range(end=df.index[-1], periods=10, freq=pd.tseries.offsets.BDay()),
+    '1 week': pd.date_range(end=df.index[-1], periods=5, freq=pd.tseries.offsets.BDay()),
+
 }
 
-fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+fig = go.Figure(data=[go.Candlestick(x=df.index,
                 open=df['Open'],
                 high=df['High'],
                 low=df['Low'],
@@ -46,7 +51,7 @@ fig = go.Figure(data=[go.Candlestick(x=df['Date'],
 
 fig.update_layout(
     height=800,
-    showlegend=False,
+    showlegend=True,
     title_text="VN30-Index Candlestick chart from 2017 to 2023",
 )
 
@@ -60,7 +65,7 @@ time_period = st.selectbox(dropdown_label, dropdown_options)
 
 # Filter data for selected time period
 start_date = time_periods[time_period][0]
-df_filtered = df["Date"].loc[start_date:]
+df_filtered = df.loc[start_date:]
 
 # Update candlestick chart data
 fig.update_traces(x=df_filtered.index,
@@ -70,21 +75,15 @@ fig.update_traces(x=df_filtered.index,
                   close=df_filtered['Close'])
 
 # ##########################################################################################
-# sma_10_trace = go.Scatter(x=df['Date'], y=df['sma_10'], name='SMA-10', visible=True)
-# sma_20_trace = go.Scatter(x=df['Date'], y=df['sma_20'], name='SMA-20', visible=True)
-# ema_10_trace = go.Scatter(x=df['Date'], y=df['ema_10'], name='EMA-10', visible=True)
-# ema_20_trace = go.Scatter(x=df['Date'], y=df['ema_20'], name='EMA-20', visible=True)
-# rsi_7_trace = go.Scatter(x=df['Date'], y=df['rsi_7'], name='RSI-7', visible=True)
-# rsi_9_trace = go.Scatter(x=df['Date'], y=df['rsi_9'], name='RSI-9', visible=True)
-# rsi_14_trace = go.Scatter(x=df['Date'], y=df['rsi_14'], name='RSI-14', visible=True)
+sma_10_trace = go.Scatter(x=df_filtered.index, y=df_filtered['sma_10'], name='SMA-10', visible=True)
+sma_20_trace = go.Scatter(x=df_filtered.index, y=df_filtered['sma_20'], name='SMA-20', visible=True)
+ema_10_trace = go.Scatter(x=df_filtered.index, y=df_filtered['ema_10'], name='EMA-10', visible=True)
+ema_20_trace = go.Scatter(x=df_filtered.index, y=df_filtered['ema_20'], name='EMA-20', visible=True)
 
-# fig.add_trace(sma_10_trace)
-# fig.add_trace(sma_20_trace)
-# fig.add_trace(ema_10_trace)
-# fig.add_trace(ema_20_trace)
-# fig.add_trace(rsi_7_trace)
-# fig.add_trace(rsi_9_trace)
-# fig.add_trace(rsi_14_trace)
+fig.add_trace(sma_10_trace)
+fig.add_trace(sma_20_trace)
+fig.add_trace(ema_10_trace)
+fig.add_trace(ema_20_trace)
 
 # # Define button label and default visibility
 # button_label = 'Show all Technical analysis indicators'
@@ -98,39 +97,67 @@ fig.update_traces(x=df_filtered.index,
 #         sma_20_trace.visible = True
 #         ema_10_trace.visible = True
 #         ema_20_trace.visible = True
-#         rsi_7_trace.visible = True
-#         rsi_9_trace.visible = True
-#         rsi_14_trace.visible = True
-#         button_label = 'Show Technical analysis indicators'
+#         button_label = 'Hide Technical analysis indicators'
 #     # Update figure layout to adjust trace visibility
 #     fig.update_layout(showlegend=True)
 
-# # Update figure layout to adjust legend and axis labels
-# fig.update_layout(
-#     legend=dict(
-#         orientation="h",
-#         yanchor="bottom",
-#         y=1.02,
-#         xanchor="right",
-#         x=1
-#     ),
-#     xaxis_title="Date",
-#     yaxis_title="Price"
-# )
+# Update figure layout to adjust legend and axis labels
+fig.update_layout(
+    legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ),
+    xaxis_title="Date",
+    yaxis_title="Price"
+)
 
 fig.update_layout(xaxis_rangeslider_visible=False)
-
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 #########################################################################################
-fig2 = go.Figure(data=[go.Table(header=dict(
-              values=["Date", "Close", "Open",
-                    "High", "Low",'sma_10', 
-                    'sma_20', 'ema_10', 'ema_20',
-                     'rsi_7', 'rsi_9', 'rsi_14']),
-              cells=dict(values=[df[k].tolist() for k in df.columns[:-1]]))
-                     ])
+rsi_7_trace = go.Scatter(x=df_filtered.index, y=df_filtered['rsi_7'], name='RSI-7', visible=True)
+rsi_9_trace = go.Scatter(x=df_filtered.index, y=df_filtered['rsi_9'], name='RSI-9', visible=True)
+rsi_14_trace = go.Scatter(x=df_filtered.index, y=df_filtered['rsi_14'], name='RSI-14', visible=True)
 
-    
+
+fig_rsi = go.Figure(data=[rsi_7_trace,rsi_9_trace,rsi_14_trace])
+
+# Update figure layout to adjust legend and axis labels
+fig_rsi.update_layout(
+    legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ),
+    xaxis_title="Date",
+    yaxis_title="Price"
+)
+
+fig_rsi.update_layout(xaxis_rangeslider_visible=False)
+st.plotly_chart(fig_rsi, theme="streamlit", use_container_width=True)
+#########################################################################################
+
+# df_2 = pd.read_csv('vn30-his-2.csv')
+# fig2 = go.Figure(data=[go.Table(header=dict(
+#               values=["Date","Close", "Open",
+#                     "High", "Low",'sma_10', 
+#                     'sma_20', 'ema_10', 'ema_20',
+#                      'rsi_7', 'rsi_9', 'rsi_14']),
+#               cells=dict(values=[df_2[k].tolist() for k in df_2.columns[:-1]]))
+#                      ])
+
+fig2 = go.Figure(data=[go.Table(
+    header=dict(values=list(df_filtered.columns)),
+    cells=dict(values=[df_filtered.index, df_filtered.Close, df_filtered.Open,
+                       df_filtered.High, df_filtered.Low, df_filtered.sma_10,
+                       df_filtered.sma_20,df_filtered.ema_10,df_filtered.ema_20,
+                       df_filtered.rsi_7, df_filtered.rsi_9, df_filtered.rsi_14]))
+])
+
 fig2.update_layout(
     height=400,
     showlegend=False,
@@ -174,11 +201,29 @@ with col11:
   st.metric(label="F1-score", value="71%")
 with col12:
   st.metric(label="Support", value="303")  
-  
+
+import datetime
+
+# d = st.date_input(
+#     "When\'s your birthday",
+#     datetime.date(2019, 7, 6))
+# st.write('Your birthday is:', d)
+
+st.header("Report model")
+cold1, cold2 = st.columns(2)
+with cold1:
+  d = st.date_input(
+    "Start: ",
+    datetime.date(2019, 7, 6))
+with cold2:
+  d2 = st.date_input(
+    "End: ",
+    datetime.date(2023, 4, 4))
+
 ########################################################################
 option = st.selectbox(
     '**How would you like to be input data?**',
-    ('Manual Input', 'Upload a file'))
+    ('Automatic','Manual Input', 'Upload a file'))
 
 if option == 'Manual Input':
 #   url = "https://finance.vietstock.vn/phan-tich-ky-thuat.htm#"
@@ -189,45 +234,45 @@ if option == 'Manual Input':
     # historical data
     his1, his2, his3 = st.columns(3)
     with his1:
-      bid_quality = st.number_input("Feature 1")
+      bid_quality = st.number_input("Number of buy orders")
     with his2:
-      bid_volume = st.number_input("Feature 2")
+      bid_volume = st.number_input("Buy orders volume")
     with his3:
-      ask_quality = st.number_input("Feature 3")
+      ask_quality = st.number_input("Number of sell orders")
       
     his4, his5, his6 = st.columns(3)
     with his4:
-      ask_volume = st.number_input("Feature 4")
+      ask_volume = st.number_input("Sell orders volume")
     with his5:
-      matching_volume = st.number_input("Feature 5")
+      matching_volume = st.number_input("Order matching volume")
     with his6:
-      negotiable_volume = st.number_input("Feature 6")
+      negotiable_volume = st.number_input("Put-through volume")
     
     st.title("Financial news")
     dum1, dum2, ta1 = st.columns(3)
     with dum1:
-      positive = st.number_input("Feature 7", value=1)
+      positive = st.number_input("Positive news", value=1)
     with dum2:
-      negative = st.number_input("Feature 8", value=0)
+      negative = st.number_input("Negative news", value=0)
     with ta1:
-      SMA_10_lag = st.number_input("Feature 9")  
+      SMA_10_lag = st.number_input("SMA 10 days")  
       
     st.title("Technical analysis")
     ta2, ta3, ta4 = st.columns(3)
     with ta2:
-      SMA_20_lag = st.number_input("Feature 10")
+      SMA_20_lag = st.number_input("SMA 20 days")
     with ta3:
-      EMA_10_lag = st.number_input("Feature 11")
+      EMA_10_lag = st.number_input("EMA 10 days")
     with ta4:
-      EMA_20_lag = st.number_input("Feature 12")
+      EMA_20_lag = st.number_input("EMA 20 days")
       
     ta5, ta6, ta7 = st.columns(3)
     with ta5:
-      RSI_7d_lag = st.number_input("Feature 13")
+      RSI_7d_lag = st.number_input("RSI 7 days")
     with ta6:
-      RSI_9d_lag = st.number_input("Feature 14")
+      RSI_9d_lag = st.number_input("RSI 9 days")
     with ta7:
-      RSI_14d_lag = st.number_input("Feature 15")
+      RSI_14d_lag = st.number_input("RSI 14 days")
     # Prediction
     features= [bid_quality, bid_volume, ask_quality, ask_volume, matching_volume, matching_volume,
                positive, negative, SMA_10_lag, SMA_20_lag, EMA_10_lag, EMA_20_lag, RSI_7d_lag, RSI_9d_lag, RSI_14d_lag]
@@ -239,43 +284,44 @@ if option == 'Manual Input':
     
     pred = scaler.predict(np.array(features,ndmin=2))
     
-    submitted = st.form_submit_button("Submit data") 
+    submitted = st.form_submit_button("Forecast buttom") 
+    check_data = False
     if submitted:     
-      if (RSI_14d_lag != 0.00):
+      if (RSI_14d_lag != 0.00 and bid_quality != 0.00 and bid_volume != 0.00 and ask_quality != 0.00 and ask_volume != 0.00 
+          and matching_volume != 0.00 and SMA_10_lag != 0.00 and SMA_20_lag != 0.00 and EMA_10_lag != 0.00 and EMA_20_lag != 0.00
+          and RSI_7d_lag != 0.00 and RSI_9d_lag != 0.00 and RSI_14d_lag != 0.00):
+        check_data = True
         with st.spinner('Wait for it...'):
-          time.sleep(3)
+          time.sleep(2)
         st.success('This is a success updating!', icon="✅")
         st.dataframe(res_df)
+        pred_out(pred)
       else:
+        check_data = False
         with st.spinner('Wait for it...'):
           time.sleep(1)
         st.warning('You do not input enough neccessary features', icon="⚠️")
     
-    submitted_2 = st.form_submit_button("Get result") 
-    if submitted_2:     
-      with st.spinner('Wait for it...'):
-        time.sleep(3)
-      pred_out(pred)
-#       st.info(pred)
+#     submitted_2 = st.form_submit_button("Get result") 
+#     if submitted_2:     
+#       with st.spinner('Wait for it...'):
+#         time.sleep(3)
+#       pred_out(pred)
+#     else:
+#       st.warning('You do not input enough neccessary features', icon="⚠️")
 
-else:
+    
+elif option == 'Upload a file':
+  sample_df = pd.DataFrame({'Number of buy orders': 66774, 'Buy-orders volume':196533544, 'Number of sell orders':58645, 'Sell-orders volume':199406752,
+                           'Order matching volume':-2872869, 'Put-through volume':107108336, 'Positive':1, 'Negative':0,
+                           'SMA_10':1020, 'SMA_20':1019, 'EMA_10':1020, 'EMA_20':1019, 'RSI_7d':56, 
+                           'RSI_9d':55, 'RSI_14d':54},index=["dd-MM-YY"])
+  st.write("Please upload data like the sample:")
+  st.dataframe(sample_df)
+
   uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
   for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-  
-st.title("Train model")
-
-
-        
-
-
-col1_1, col2_1, = st.columns(2)
-with col1_1:
-  st.metric(label="VN30-Index", value= "14-04-2023")
-with col2_1:
-  st.metric(label="Trend Prediction", value="80%")
-
-
-
-
+else:
+    pass
