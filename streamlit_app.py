646dcb3039739c214af5ebc0f91c2f3c0ef9b48b
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-17 00:35:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -325,13 +325,12 @@ if select_event == 'Manual input':
                            'SMA_10':SMA_10_lag, 'SMA_20':SMA_20_lag, 'EMA_10':EMA_10_lag, 'EMA_20':EMA_20_lag, 'RSI_7d':RSI_7d_lag, 
                            'RSI_9d':RSI_9d_lag, 'RSI_14d':RSI_14d_lag},index=["21-04-2023"])
     
-    check = st.sidebar.button('Submit data')
-    if check:
+    if st.sidebar.button('Submit data'):
         with st.spinner('Wait for it...'):
             time.sleep(2)
             st.success('This is a success updating!', icon="✅")
             st.dataframe(res_df)
-            pred_out(pred)
+#             pred_out(pred)
     else:
         check_data = False
         with st.spinner('Wait for it...'):
@@ -339,10 +338,7 @@ if select_event == 'Manual input':
             st.warning('You do not input enough neccessary features', icon="⚠️")
     
 #     pred = scaler.predict(np.array(features,ndmin=2))
-    
 
-#     submitted = st.form_submit_button("Forecast buttom") 
-#     check_data = False
 #     submitted = st.form_submit_button("Forecast buttom") 
 #     check_data = False
 #     if submitted:     
