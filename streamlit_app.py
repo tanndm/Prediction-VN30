0bdb1b4afedb778037d39c9e4e7d1b98d274117f
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-17 11:54:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -229,7 +229,7 @@ if select_event == 'Manual input':
         st.success('This is a success updating!', icon="✅")
         st.dataframe(res_df)
         pred_out(pred)
-        df_prob = pd.DataFrame({'Downtrend':pred_prob[0], 'Uptrend':pred_prob[1]},index=["21-04-2023"])
+        df_prob = pd.DataFrame({'Downtrend':pred_prob[:,0], 'Uptrend':pred_prob[:,1]},index=["21-04-2023"])
         st.dataframe(df_prob)
     else:
         with st.spinner('Wait for it...'):
