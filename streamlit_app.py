3916a3b75a43ce4b2562d9be771fd1933d84aaf4
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-19 14:08:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -252,7 +252,8 @@ elif select_event == 'Upload file':
   for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.sidebar.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    file_new = load_data(uploaded_file)
+    st.dataframe(file_new)
     
 
 
