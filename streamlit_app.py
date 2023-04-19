d80903ae7c42cccace7e9c23821887577fd346fb
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-19 16:09:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,29 +10,16 @@ from io import StringIO
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://www.uel.edu.vn" target="_blank">
-  <img class="image-25"src="https://www.uel.edu.vn/Resources/Images/SubDomain/HomePage/Style/logo_uel.png" width="1000"
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-      </li>
-    </ul>
-  </div>
-</nav>
-""", unsafe_allow_html=True)
-
-
-# # -- Set page config
-# apptitle = 'Predict VN30-index price movement using financial news and technical analysis'
-# st.set_page_config(page_title=apptitle, 
+# -- Set page config
+apptitle = 'Predict VN30-index price movement using financial news and technical analysis'
+st.set_page_config(page_title=apptitle, layout="centered",initial_sidebar_state = "auto",
 #                    page_icon="chart_with_upwards_trend",
-#                    layout="wide")
+                   page_icon="🧊", layout="wide",
+                  menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    })
 
 # # Unpacking Scaler pkl file
 S_file = open('model_rf.pkl','rb')
