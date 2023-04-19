6e15f463c1e05ad7318945c9c74644967671c6a9
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-19 17:27:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,7 +138,7 @@ fig2.update_layout(
 )
 
 
-st.image('Chi-so-VN30-la-gi.jpeg', width=200)
+st.sidebar.image('Chi-so-VN30-la-gi.jpeg', width=100)
 st.sidebar.markdown('#### Support tool')
 st.sidebar.markdown('#### VN30-Index data table from 2017 to 2023')
 click_data = st.sidebar.checkbox('Click here to show out all of historical data of VN30-Index', value=False)
