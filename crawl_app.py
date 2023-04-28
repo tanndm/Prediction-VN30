befcdd0d45e3e7df777a89fa37cb3d8ed9c10331
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-28 10:20:42 +0700

    Update crawl_app.py

--- crawl_app.py
+++ crawl_app.py
@@ -33,21 +33,21 @@ if url:
     for href in link_list:
       if ("https://" in href) and ('-' in href):
         else_list.append(href)
-    st.write(else_list)   
+    st.write(else_list)  
+    title_list = []
     for i in else_list:
       response_2 = requests.get(i)
       soup_2 = BeautifulSoup(response_2.content, 'html.parser')
     # Extract the data you want to display
-      title_list = []
       title = soup_2.title.string
       title_list.append(title)
     st.write(title_list)
     
-#     translate = Translator()
-#     en_lst = []
-#     for j in title_list:
-#       trans = translate.translate(j, src='vi', dest='en')
-#       en_lst.append(trans.text)
-#       st.write(trans.text)
-    # Display the data using Streamlit
-#     st.title(title_list)
+    translate = Translator()
+    en_lst = []
+    for j in title_list:
+      trans = translate.translate(j, src='vi', dest='en')
+      en_lst.append(trans.text)
+      st.write(trans.text)
+    Display the data using Streamlit
+    st.title(title_list)
