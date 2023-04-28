43acdfe12ccf6c032df491af2f2ca1d10a80ec97
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-28 09:01:24 +0700

    Update crawl_app.py

--- crawl_app.py
+++ crawl_app.py
@@ -4,5 +4,24 @@ import numpy as np
 import joblib
 # Importing all required modules 
 import webbrowser as wb
+import requests
+from bs4 import BeautifulSoup
 
-x = st.slider('Select a value')
+# -- Set page config
+apptitle = 'Predict VN30-index crawl data'
+st.set_page_config(page_title=apptitle, 
+                   layout="wide",
+                   page_icon="🧊",
+                   initial_sidebar_state="expanded")
+#                  page_icon="chart_with_upwards_trend")
+
+# Crawl the webpage using requests
+url = 'https://vnexpress.net/thanh-tich-kinh-te-khong-tron-ven-cua-ong-biden-4598764.html'
+response = requests.get(url)
+# Parse the HTML using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Extract the data you want to display
+title = soup.title.string
+# Display the data using Streamlit
+st.title(title)
