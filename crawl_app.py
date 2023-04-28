439b55c89505cdf4724a4814f8ae08efb55903c7
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-28 16:45:56 +0700

    Update crawl_app.py

--- crawl_app.py
+++ crawl_app.py
@@ -66,5 +66,5 @@ response_test = requests.get("https://vnexpress.net/khoi-ngoai-lien-tiep-mua-ron
 soup_test = BeautifulSoup(response_test.content, 'html.parser')
 span_element = soup_test.find('span', {'xpath': '/html/body/section[5]/div/div[2]/div[1]'})
 # Get the text content of the span element
-span_text = span_element
+span_text = span_element.text
 st.write(span_text)
