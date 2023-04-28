eeb331161e85774ad060b7932693efb080ae9562
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-04-28 09:28:09 +0700

    Update crawl_app.py

--- crawl_app.py
+++ crawl_app.py
@@ -15,13 +15,29 @@ st.set_page_config(page_title=apptitle,
                    initial_sidebar_state="expanded")
 #                  page_icon="chart_with_upwards_trend")
 
-# Crawl the webpage using requests
-url = 'https://vnexpress.net/kinh-doanh/chung-khoan-p2'
-response = requests.get(url)
-# Parse the HTML using BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+# # Crawl the webpage using requests
+# url = 'https://vnexpress.net/kinh-doanh/chung-khoan-p2'
+# response = requests.get(url)
+# # Parse the HTML using BeautifulSoup
+# soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract the data you want to display
-title = soup.title.string
-# Display the data using Streamlit
-st.title(title)
+# # Extract the data you want to display
+# title = soup.title.string
+# # Display the data using Streamlit
+# st.title(title)
+
+
+# Get the URL from the user using a text input widget
+url = st.text_input('Enter a URL')
+
+# Extract the href attribute from the link using requests and BeautifulSoup
+if url:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = soup.find_all('a')
+    hrefs = [link.get('href') for link in links]
+
+    # Display the extracted href attributes using a write widget
+    st.write('Extracted href attributes:')
+    for href in hrefs:
+        st.write(href)
