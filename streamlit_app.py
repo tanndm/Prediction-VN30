b12aad10627ce2da10e2cac8452ad4038da2ba34
Nguyen Duc Minh Tan <82720197+BrianNguyen2001@users.noreply.github.com>
2023-05-14 00:00:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -285,7 +285,9 @@ elif select_event == 'Upload file':
     st.warning('You do not input neccessary features', icon="⚠️")
 elif select_event == 'Link Github':
   link_git = st.sidebar.text_input("Enter your link:")
-  df_link = pd.read_csv(link_git)
+  st.write(link_git)
+#   df_link = pd.read_csv(link_git)
+#   pd.read_csv(r'https://raw.githubusercontent.com/BrianNguyen2001/Prediction-VN30/main/cleaned_new_data_datn.csv')
   st.dataframe(df_link)
 elif select_event == 'Select':
   pass
